2c00bce187712509f9b845ba4c2c6e379ef6cf83
Erick <ericklima.ca@yahoo.com>
2021-08-05 23:38:34 -0400

    update

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 # python 3.9.6
 
-from flask import Flask
-from config import app_config, app_active
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from config import app_config, app_active
+from controller.User import UserController
+from admin.Admin import start_views
+
 
 config = app_config[app_active]
 
@@ -15,6 +18,7 @@ def create_app(config_name):
     app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(config.APP)
+    start_views(app, db)
     db.init_app(app)
 
 
@@ -26,8 +30,46 @@ def create_app(config_name):
     def login():
         return 'Aqui entrará a tela de login...'
 
+    @app.route('/login/', methods = ['POST'])
+    def login_post():
+        user = UserController()
+        email = request.form['email']
+        password = request.form['password']
+        result = user.login(email, password)
+        if result:
+            return redirect('/admin/')
+        else:
+            return render_template(
+                'login.html',
+                data = {
+                'status': 401,
+                'msg': 'Dados de usuário incorretos!',
+                'type': None
+            })
+
     @app.route('/recovery_password/')
     def recovery_password():
         return 'Aqui entrará a tela de recuperação de senha...'
+    
+    @app.route('/recovery_password/', methods = ['POST'])
+    def send_recovery_password():
+        user = UserController()
+        result = user.recovery(request.form['email'])
+        if result:
+            return render_template(
+                'recovery.html',
+                data = {
+                    'status': 200,
+                    'msg': 'E-mail de verificação enviado com sucesso!'
+                }
+            )
+        else:
+            return render_template(
+                 'recovery.html',
+                data = {
+                    'status': 401,
+                    'msg': 'Erro ao enviar e-mail de verificação!'
+                }               
+            )
 
     return app
--- model/Category.py
+++ model/Category.py
@@ -2,6 +2,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from config import app_config, app_active
+from model.User import User
 
 config = app_config[app_active]
 db = SQLAlchemy(config.APP)
@@ -9,4 +10,7 @@ db = SQLAlchemy(config.APP)
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(20), unique= True, nullable = False)
-    description = db.Column(db.Text(), nullable = False)
+    description = db.Column(db.Text(), nullable = True)
+    
+    def __repr__(self):
+        return self.name
